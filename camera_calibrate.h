4a4baf02861030e8815ee178becaf95459335acc
horizon <liudxp@live.cn>
2020-04-11 16:52:41 +0800

    update

---
+++ camera_calibrate.h
@@ -0,0 +1,16 @@
+#ifndef CAMERA_CALIBRATE
+#define CAMERA_CALIBRATE
+
+#ifdef __cplusplus
+
+#include <opencv4/opencv2/calib3d.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CAMERA_CALIBRATE
