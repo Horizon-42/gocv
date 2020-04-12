dd3804a84a2055c038c679b7e3056f4aa8a60262
liu_dongxu <ldx@xiaofutech.com>
2020-04-12 19:10:39 +0800

    debug

--- camera_calibrate.go
+++ camera_calibrate.go
@@ -10,8 +10,14 @@ import "image"
 // 内参外参数标定相关函数
 
 func GetInternalMat(pics []Mat, patternSize image.Point, cameraMatrix, distCoffs *Mat) float64 {
+	cMatArray:=make([]C.Mat, len(pics))
+
+	for i, r := range pics {
+		cMatArray[i] = r.p
+	}
+
 	cMats := C.struct_Mats{
-		mats:   (*C.Mat)(&pics[0]),
+		mats:   (*C.Mat)(&cMatArray[0]),
 		length: C.int(len(pics)),
 	}
 
@@ -26,17 +32,28 @@ func GetBMat(pics, cameraMatrix, distCoffs []Mat, patternSize image.Point, B *Ma
 	if len(pics) != 2 || len(cameraMatrix) != 2 || len(distCoffs) != 2 {
 		panic("input error")
 	}
+
+	cPicArray := make([]C.Mat, 2)
+	cCameraMatArray := make([]C.Mat, 2)
+	cDistCoffsArray := make([]C.Mat, 2)
+
+	for i := 0; i < 2; i++ {
+		cPicArray[i] = pics[i].p
+		cCameraMatArray[i] = cameraMatrix[i].p
+		cDistCoffsArray[i] = distCoffs[i].p
+	}
+
 	cPics := C.struct_Mats{
-		mats:   (*C.Mat)(&pics[0]),
-		length: C.int(len(pics)),
+		mats:   (*C.Mat)(&cPicArray[0]),
+		length: C.int(2),
 	}
 	cCameraMats := C.struct_Mats{
-		mats:   (*C.Mat)(&cameraMatrix[0]),
-		length: C.int(len(cameraMatrix)),
+		mats:   (*C.Mat)(&cCameraMatArray[0]),
+		length: C.int(2),
 	}
 	cDistCoffs := C.struct_Mats{
-		mats:   (*C.Mat)(&distCoffs[0]),
-		length: C.int(len(distCoffs)),
+		mats:   (*C.Mat)(&cDistCoffsArray[0]),
+		length: C.int(2)),
 	}
 	sz := C.struct_Size{
 		width:  C.int(patternSize.X),
