d2058faa408f069e20583cdf58c8b6bb62c3348a
horizon <liudxp@live.cn>
2020-09-23 19:58:53 +0800

    ud

--- camera_calibrate.h
+++ camera_calibrate.h
@@ -6,23 +6,26 @@
 #include <opencv4/opencv2/calib3d.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 
-bool GetExternalMat(const cv::Mat &pic, const cv::Mat &cameraMatrix, const cv::Mat &distCoffs,
-                    cv::Size patternSize, cv::Mat &external);
+bool GetExternalMat(const cv::Mat &pic, const cv::Mat &cameraMatrix,
+                    const cv::Mat &distCoffs, cv::Size patternSize,
+                    cv::Mat &external);
 
-extern "C"
-{
+extern "C" {
 #endif
 #include "core.h"
 
-    double GetInternalMat(Mats pics, Size patternSize, Mat cameraMatrix,
-                          Mat distCoffs, bool accuracy);
-    bool GetBMat(Mats pics, Mats cameraMatrix, Mats distCoffs, Size patternSize,
-                 Mat B);
+double GetInternalMat(Mats pics, Size patternSize, Mat cameraMatrix,
+                      Mat distCoffs, bool accuracy);
+bool GetBMat(Mats pics, Mats cameraMatrix, Mats distCoffs, Size patternSize,
+             Mat B);
 
+bool GetStereoBMat();
 
-    bool GetEMat(Mat pic, Mat cameraMatrix, Mat distCoffs, Size patternSize, Mat externalMat);
+bool GetEMat(Mat pic, Mat cameraMatrix, Mat distCoffs, Size patternSize,
+             Mat externalMat);
 
-    bool ProjectPoints(Mat objectPoints, Mat externalMat, Mat cameraMatrix, Mat distCoeffs, Mat imagePoints);
+bool ProjectPoints(Mat objectPoints, Mat externalMat, Mat cameraMatrix,
+                   Mat distCoeffs, Mat imagePoints);
 
 #ifdef __cplusplus
 }
